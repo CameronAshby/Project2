df1233f7bc6f2884588464140341e0b26e423c66
Cameron Ashby <c00ashby@gmail.com>
2018-10-19 13:54:23 -0600

    re-working addList function

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,31 +1,63 @@
 let listArray = [];
+let listIdCounter = 0;
 
-class Store {
-    constructor(listIndex, listContent) {
-        this.listIndex = listIndex;
-        this.listContent = listContent;
-    }
-    static storeList(listIndex, listContent) {
-        let listString = JSON.stringify(listContent);
-        localStorage.setItem(listIndex, listString);
-    };
-    static changeList(listIndex)  {
-        let listContent = localStorage.getItem(listIndex);
-        return JSON.parse(listContent);
-    };
-    static clearList(listIndex) {
-        localStorage.removeItem(listId);
-    };
-}
+// class Store {
+//     constructor(listIndex, listContent) {
+//         this.listIndex = listIndex;
+//         this.listContent = listContent;
+//     }
+//     static storeList(listIndex, listContent) {
+//         let listString = JSON.stringify(listContent);
+//         localStorage.setItem(listIndex, listString);
+//     };
+//     static changeList(listIndex)  {
+//         let listContent = localStorage.getItem(listIndex);
+//         return JSON.parse(listContent);
+//     };
+//     static clearList(listIndex) {
+//         localStorage.removeItem(listId);
+//     };
+// }
 
 function addList() {
+    let listName;
+
     $('.listBar').append(
-        '<div>Test</div>'
-    )
+        '<div id="' + listIdCounter +'" class="newListContainer" style="display: flex;">' +
+            '<input id="input' + listIdCounter + '" type="text" placeholder="List Name" onkeydown="getVal(this.value)">' +
+            '<div><i onclick="buildList()" class="editList fas fa-edit"></i><i onclick="trash(this.id)" class="trashBin fas fa-trash-alt"></i></div>' +
+        '</div>'
+    );
+
+    class newList {
+        constructor(listId, listName) {
+            this.tasks = [];
+            this.listId = listId;
+            this.listName = listName
+        }
+    };
+
+    // let listName = prompt('Enter the name of your list');
+
+    $('#' + listIdCounter).append(listName);
+
+    listArray.push($('#' + listIdCounter));
+    listIdCounter++;
+}
+
+function buildList() {
+    $('.listContent').append(
+        '<button class="listItem" onclick="addListItem()">Add Task<i class="fas fa-plus-circle"></i></button>'
+    );
 }
 
 function addListItem() {
+    $('.listContent').append(
+        '<div id="test">Test<i onclick="" class="fas fa-trash-alt"></i></div>'
+    );
 
+    listArray.push($('#test'));
+    listIdCounter++;
 }
 
 function clearList() {
@@ -42,4 +74,12 @@ function storeList() {
 
 function changeList() {
 
+}
+
+function trash(myId) {
+
+}
+
+function getVal(listValue) {
+    return listValue;
 }
