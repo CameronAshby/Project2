a4980ceacda0e664b4c5308ea0edb4f9301c5ec6
Cameron Ashby <c00ashby@gmail.com>
2018-10-20 16:39:09 -0600

    fixed functionality issues

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -34,12 +34,11 @@ class newList {
 // Adding Functions
 
 function addList() {
-    console.log(listIdCounter);
     listArray.push(new newList());
 
     $('.listBar').append(
         '<div class="listEditContainer" id="' + listIdCounter +'">' +
-        '<div class="icons"><button type="button" class="btn" id="deleteList' + listIdCounter + '" onclick="deleteLists(this.id)"><i class="trash fas fa-trash-alt"></i></button><button type="button" class="btn" onclick="editList(this.id)" id="button' + listIdCounter +'"><i class="edit fas fa-edit"></i></button></div>' +
+        '<div class="icons"><button type="button" class="btn" id="deleteList_' + listIdCounter + '" onclick="deleteLists(this.id)"><i class="trash fas fa-trash-alt"></i></button><button type="button" class="btn" onclick="editList(this.id)" id="button' + listIdCounter +'"><i class="edit fas fa-edit"></i></button></div>' +
         '<input id="input' + listIdCounter + '" type="text" placeholder="Enter List Name" onchange="getListName(this.value, this.id)">' +
         '</div>'
     );
@@ -54,20 +53,18 @@ function getListName(listVal, listId) {
 }
 
 function editList(buttonId) {
-    console.log(buttonId);
     let newId = buttonId.split('n');
     activeList = listArray[newId[1]];
     displayTasks();
 }
 
 function displayLists() {
-    console.log(listIdCounter);
     $('.listBar').html('');
     if(listArray.length !== 0) {
         for(let i = 0; i < listArray.length; i++) {
             $('.listBar').append(
                 '<div class="listEditContainer" id="' + i +'">' +
-                '<div class="icons"><button type="button" class="btn" id="deleteList'+listIdCounter+'" onclick="deleteLists(this.id)"><i class="trash fas fa-trash-alt"></i></button><button type="button" class="btn" onclick="editList(this.id)" id="button' + listIdCounter +'"><i class="edit fas fa-edit"></i></button></div>' +
+                '<div class="icons"><button type="button" class="btn" id="deleteList_'+i+'" onclick="deleteLists(this.id)"><i class="trash fas fa-trash-alt"></i></button><button type="button" class="btn" onclick="editList(this.id)" id="button' + i +'"><i class="edit fas fa-edit"></i></button></div>' +
                 '<input id="input' + i + '" type="text" onchange="getListName(this.value, this.id)" value="'+ listArray[i].listName +'">' +
                 '</div>'
             );
@@ -82,8 +79,8 @@ function displayTasks() {
         for(let i = 0; i < activeList.tasks.length; i++) {
             $('.listContent').append(
                 '<div class="taskContainer">' +
-                '<input id="task' + i +'" type="text" onchange="getTaskName(this.value, this.id)" value="' + activeList.tasks[i] + '">' +
-                '<button id="deleteTask' + i +'" onclick="deleteTasks(this.id)"><i class="trash fas fa-trash-alt"></i></button></div>'
+                '<input id="task_' + i +'" type="text" onchange="getTaskName(this.value, this.id)" value="' + activeList.tasks[i] + '">' +
+                '<button id="deleteTask_' + i +'" onclick="deleteTasks(this.id)"><i class="trash fas fa-trash-alt"></i></button></div>'
             );
 
             if (activeList.taskCompleted[i] == true) {
@@ -108,12 +105,12 @@ function addTasks() {
     activeList.taskCompleted.push(false);
 
     $('.listContent').append(
-        '<input id="task' + (activeList.tasks.length-1) + '" type="text" placeholder="Enter Task Name" onchange="getTaskName(this.value, this.id)">'
+        '<input id="task_' + (activeList.tasks.length-1) + '" type="text" placeholder="Enter Task Name" onchange="getTaskName(this.value, this.id)">'
     );
 }
 
 function getTaskName(taskVal, taskId) {
-    let index = taskId.split('k');
+    let index = taskId.split('_');
     activeList.tasks[index[1]] = taskVal;
     displayTasks();
 }
@@ -121,14 +118,15 @@ function getTaskName(taskVal, taskId) {
 // Deleting Functions
 
 function deleteTasks(taskId) {
-    let index = taskId.split('k');
+    let index = taskId.split('_');
     activeList.tasks.splice(index[1], 1);
     activeList.taskCompleted.splice(index[1], 1);
     displayTasks();
 }
 
 function deleteLists(listId) {
-    let index = listId.split('t');
+    console.log(listId);
+    let index = listId.split('_');
     listArray.splice(index[1], 1);
     displayLists();
     $('.listContent').html('');
